02cd378bc9acdd8665b4f6f43771050faec65c6e
john-raber <jrab224@gmail.com>
2018-10-02 15:57:12 -0400

    added dependent: :destroy on trip and place_trip models so that join table records are destroyed when we delete a trip or place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :city
-  has_many :place_trips
+  has_many :place_trips, dependent: :destroy
   has_many :trips, through: :place_trips
   has_many :users, through: :trips
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :place_trips
+  has_many :place_trips, dependent: :destroy
   has_many :places, through: :place_trips
 end
